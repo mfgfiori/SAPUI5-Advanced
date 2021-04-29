896c54a29afb2c3b3e441e73c9e25e658193d86d
mfgfiori <mfdezgarcia@gmail.com>
2021-04-29 18:50:52 +0000

    Lab_Componentes_Dinamicos

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -77,6 +77,87 @@ sap.ui.define([
             oJSONModelConfig.setProperty("/visibleBtnHideCity", false);
         };
 
+        function showOrders(oEvent){
+            var ordersTable = this.getView().byId("ordersTable");
+
+            ordersTable.destroyItems(); 
+
+            var itemPressed = oEvent.getSource();
+            var oContext = itemPressed.getBindingContext("jsonEmployees");
+
+            var objectContext = oContext.getObject();
+            var orders = objectContext.Orders;
+
+            var ordersItems = [];
+
+            for (var i in orders) {
+                ordersItems.push(new sap.m.ColumnListItem({
+                    cells : [
+                        new sap.m.Label({ text: orders[i].OrderID}),
+                        new sap.m.Label({ text: orders[i].Freight}),
+                        new sap.m.Label({ text: orders[i].ShipAddress})
+                    ]
+                }));
+            }
+            var newTable = new sap.m.Table({
+                width: "auto",
+                columns:[
+                    new sap.m.Column({header : new sap.m.Label({text: "{i18n>orderId}"})}),
+                    new sap.m.Column({header : new sap.m.Label({text: "{i18n>freight}"})}),
+                    new sap.m.Column({header : new sap.m.Label({text: "{i18n>shipAddress}"})})
+                ],
+                items: ordersItems
+            }).addStyleClass("sapUiSmallMargin");
+
+            ordersTable.addItem(newTable);
+
+            var newTableJSON = new sap.m.Table();
+            newTableJSON.setWidth("auto");
+            newTableJSON.addStyleClass("sapUiSmallMargin");
+
+            var columnOrderID = new sap.m.Column();
+            var labelOrderId = new sap.m.Label();
+            labelOrderId.bindProperty("text", "i18n>orderId");
+            columnOrderID.setHeader(labelOrderId);
+            newTableJSON.addColumn(columnOrderID);
+
+            var columnFreight = new sap.m.Column();
+            var labelFreight = new sap.m.Label();
+            labelFreight.bindProperty("text", "i18n>freight");
+            columnFreight.setHeader(labelFreight);
+            newTableJSON.addColumn(columnFreight);
+
+            var columnShipAddress = new sap.m.Column();
+            var labelShipAddress = new sap.m.Label();
+            labelShipAddress.bindProperty("text", "i18n>shipAddress");
+            columnShipAddress.setHeader(labelShipAddress);
+            newTableJSON.addColumn(columnShipAddress);
+
+            var columnListItem = new sap.m.ColumnListItem();
+
+            var cellOrderId = new sap.m.Label();
+            cellOrderId.bindProperty("text", "jsonEmployees>OrderID");
+            columnListItem.addCell(cellOrderId);
+
+            var cellFreight = new sap.m.Label();
+            cellFreight.bindProperty("text", "jsonEmployees>Freight");
+            columnListItem.addCell(cellFreight);
+
+            var cellShipAddress = new sap.m.Label();
+            cellShipAddress.bindProperty("text", "jsonEmployees>ShipAddress");
+            columnListItem.addCell(cellShipAddress);
+
+            var oBindingInfo ={
+                model : "jsonEmployees",
+                path : "Orders",
+                template: columnListItem
+            };
+
+            newTableJSON.bindAggregation("items", oBindingInfo);
+            newTableJSON.bindElement("jsonEmployees>" + oContext.getPath());
+            ordersTable.addItem(newTableJSON);
+        };
+
         const Main = Controller.extend("logaligroup.Employees.controller.MainView", {});
 
         Main.prototype.onValidate = function () {
@@ -98,6 +179,7 @@ sap.ui.define([
         Main.prototype.onFilter = onFilter;
         Main.prototype.onClearFilter = onClearFilter;
         Main.prototype.onShowCity = onShowCity;
-        Main.prototype.onHideCity = onHideCity
+        Main.prototype.onHideCity = onHideCity;
+        Main.prototype.showOrders = showOrders;
         return Main;
     });
